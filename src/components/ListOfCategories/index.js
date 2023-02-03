76aa1136fa2aa6e0fc3720d2c060277347df1d85
LauraChinchilla <108949665+LauraKChinchilla@users.noreply.github.com>
2023-02-03 16:59:49 -0600

    Update index.js

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import { Category } from '../Category'
+
 import { List, Item } from './styles'
 import data  from '../../../api/db.json'
+
 export const ListOfCategories = ()=>{
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+
+  const [showFixed, setShowFixed] = useState
+  (false)
+
+  useEffect(function () {
+    window.fetch('https://curso-react-r3lhe9l6u-karichinchilla34-gmailcom.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
+  useEffect(function(){
+    const onScroll = e =>{
+      const newShowFixed=window.scrollY > 
+      200
+    showFixed !== newShowFixed && 
+    setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll',
+    onScroll)
+
+    return()=>document.removeEventListener('scroll',onScroll)},[showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
-        data.categories.map(category=><Item key={category.id}><Category {...category} /></Item>)
+      data.categories.map(category=><Item key={category.id}
+      ><Category {...category} /></Item>)
       }
     </List>
   )
+
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment> 
+  )
 }
+
+
