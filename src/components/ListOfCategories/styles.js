f8a85241b440f58a5c9f8a3076fcf6299bf57af9
LauraChinchillaCh <laura@abstractdev.studio>
2023-02-04 22:00:42 -0600

    Update styles.js

--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,9 +1,10 @@
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 export const List=styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
-  &.fixed {
+  ${props => props.fixed && css`
+  {
     background: #fff;
     border-radius: 60px;
     box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
@@ -17,6 +18,8 @@ export const List=styled.ul`
     transform: scale(.5);
     z-index: 1;
   }
+  `}
+
 `
 export const Item=styled.li`
   padding: 0 8px;
