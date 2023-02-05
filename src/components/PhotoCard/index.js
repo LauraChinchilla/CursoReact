6b0aeaa927724e00ef759e619879845d08efaadf
LauraChinchillaCh <laura@abstractdev.studio>
2023-02-04 22:00:48 -0600

    Update index.js

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,22 +1,73 @@
-import React from "react";
-import { ImgWrapper, Img, Button } from "./styles";
-import { MdFavoriteBorder } from 'react-icons/md'
+import React, {Fragment, useEffect, useRef, useState} from "react";
+import { Article, ImgWrapper, Img, Button } from "./styles";
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { useNearScreen } from "../../hooks/useNearScreen";
 
 const DEFAULT_IMAGE = "https://images.unsplash.com/photo-1500879747858-bb1845b61beb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+
+
+
 export const PhotoCard = ({ id, likes =0,
 src = DEFAULT_IMAGE }) => {
+
+    const [show, element] = useNearScreen()
+    
+    
+    const key = `like-${id}`
+    const [liked, setLiked] = useLocalStorage(key, false)
+
+    
+
+    console.log(liked)
+    
+    useEffect(function(){
+        Promise.resolve(
+        typeof window.IntersectionObserver !=
+        'undefined'
+            ? window.IntersectionObserver
+            : import('intersection-observer')
+            ).then(()=>{
+                const observer = new
+                window.IntersectionObserver(function
+            (entries){
+                const { isIntersecting } = entries[0]
+                
+                if (isIntersecting) {
+                   
+                    setShow(true)
+                    observer.disconnect()
+                }
+                console.log({ isIntersecting })
+            })
+            observer.observe(element.current)
+
+
+            })
+        
+    }, [element])
+
+    const Icon = liked ? MdFavorite : MdFavoriteBorder
+
+
     return (
-        <article>
-            <a href={`/detail/${id}`}>
-                <ImgWrapper>
+        <Article ref={element}>
+            {
+                show &&  <Fragment>
+                    <a href={`/detail/${id}`}>
+                    <ImgWrapper>
                     <Img src= {src}/>
-                </ImgWrapper>
-            </a>
+                    </ImgWrapper>
+                     </a>
 
-            <Button>
-                <MdFavoriteBorder size='32px'/>{likes} likes!
-            </Button>
+                     <Button onClick={() => setLiked(!liked) }>
+                      <Icon size='32px'/>{likes} likes!
+                    </Button>
 
-        </article>
+                </Fragment>
+            }
+            
+
+        </Article>
     )
 }
