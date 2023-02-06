e619cfb6bf49d915dfadb8b7d5d2505504a258f7
LauraChinchillaCh <laura@abstractdev.studio>
2023-02-06 10:48:07 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
 
-export const App = () => (
-  <div>
+export const App = () => {
+  const urlParams = new window.URLSearchParams
+  (window.location.search)
+  const detailId = urlParams.get('detail')
+  console.log(detailId)
+
+  return (
+    <div>
     <GlobalStyle />
     <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards />
+    {
+      detailId
+      ? <h1>Detail Id</h1>
+      : <Fragment>
+        <ListOfCategories />
+        <ListOfPhotoCards />
+      </Fragment>
+    }
   </div>
-  
-)
+  )
+}
